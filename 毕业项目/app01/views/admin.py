a10421fd15d27118f0a1a139a1031d456ac7bf2a
西芹 <1815750296@qq.com>
2022-04-07 17:56:15 +0800

    模态框
    实现了模态框，可以使用模态框对管理员账号进行修改密码和退出提醒，记得了使用name和value进行组合，而不是ID

--- 毕业项目/app01/views/admin.py
+++ 毕业项目/app01/views/admin.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django import forms
 
 # Create your views here.
@@ -7,7 +9,7 @@ from app01.utils.encrypt import md5
 from app01 import models
 
 def index(request):
-    return render(request,'m-index.html')
+    return render(request, 'index.html')
 
 
 class LoginForm(BootStrapForm):
@@ -19,11 +21,16 @@ class LoginForm(BootStrapForm):
         return md5(pwd)
 
 
+# class EditForm(BootStrapForm):
+#     class Meta:
+#         model=models.Admin
+#         fields=['password']
+
 def login(request):
     """登录"""
     if request.method == "GET":
         form = LoginForm()
-        return render(request, "m-login.html", {"form": form})
+        return render(request, "login.html", {"form": form})
 
     form = LoginForm(data=request.POST)
     if form.is_valid():
@@ -31,16 +38,26 @@ def login(request):
         admin_object = models.Admin.objects.filter(**form.cleaned_data).first()
         if not admin_object:
             form.add_error("password", "用户名或密码错误")
-            return render(request, "m-login.html", {"form": form})
+            return render(request, "login.html", {"form": form})
         # 网站生成随机字符串，写到用户浏览器中，再写入到session中；
         request.session['info'] = {"id": admin_object.id, "name": admin_object.username}
         # 重新设置超时时间，7天免登陆
         request.session.set_expiry(60 * 60 * 24 * 7)
         return redirect("/manage/index/")
 
-    return render(request,'m-login.html',{"form": form})
+    return render(request, 'login.html', {"form": form})
 
 def logout(request):
     """注销"""
     request.session.clear()
     return redirect("/manage/login/")
+
+
+@csrf_exempt
+def edit(request):
+    """编辑密码（Ajax请求）"""
+    uid = request.POST.get("uid")
+    pwd = request.POST.get("pwd")
+    models.Admin.objects.filter(id=uid).update(password=md5(pwd))
+    print(uid,pwd,md5(pwd))
+    return JsonResponse({"status": True})
