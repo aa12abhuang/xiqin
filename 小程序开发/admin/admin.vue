3fdda39c28a0b7faecdce13b08970e64a75ecd2e
西芹 <1815750296@qq.com>
2022-04-27 10:12:49 +0800

    模态框显示
    对删除增加了模态框

--- 小程序开发/admin/admin.vue
+++ 小程序开发/admin/admin.vue
@@ -5,6 +5,17 @@
 				<text class="cuIcon-title text-orange"></text> 新增账号/查看修改
 			</view>
 		</view>
+		<view class="box">
+			<view class="cu-bar btn-group">
+				<button class="cu-btn bg-green shadow-blur round" @click="clickcreateID()">新建账号</button>
+				<button class="cu-btn bg-blue shadow-blur round" @click="clickcreateGroup()">新建小组</button>
+			</view>
+		</view>
+		<!--弹窗-->
+		<modal v-if="showPop" title="输入新小组名" confirm-text="确定" cancel-text="取消" @cancel="cancelPop"
+			@confirm="confirmPop">
+			<input type='text' placeholder="请输入密码" v-model="newgroupname" />
+		</modal>
 		<!-- 	<scroll-view scroll-x class="bg-white nav text-center">
 			<view class="cu-item" :class="index==TabCur?'text-blue cur':''" 
 			v-for="(item,index) in funclist" :key="index" @tap="tabSelect" :data-id="index">
@@ -24,30 +35,49 @@
 							<!-- <button class="cu-btn bg-green shadow" @tap="showModal" data-target="menuModal">设置</button> -->
 						</view>
 					</view>
-
-					<view class="cu-list menu" :class="[menuBorder?'sm-border':'',menuCard?'card-menu margin-top':'']">
-
-						<view class="cu-item" :class="menuArrow?'arrow':''" v-for="(item,index) in IDlist" :key="id">
-							<view class="content">
-								<text class="cuIcon-btn text-green"></text>
-								<text class="text-grey">{{item.name}}</text>
-								<text class="text-grey" style="margin-left: 30rpx;">{{item.accout}}</text>
-							</view>
+					<view v-for="(group,index) in grouplist" :key="index">
+						<view class="cu-bar bg-white solid-bottom margin-top">
+							<text class=" " style="position: absolute;left: 50rpx;size: 40rpx;font-weight: 600;"></text>
+							{{group}}
 							<view class="action">
-								<button class="cu-btn round bg-green shadow" @click="clickchangeID(item.accout)">
-									<text class="cuIcon-upload"></text> 修改</button>
-<!-- 								<button class="cu-btn round bg-green shadow" @click="clickremoveID(item.accout)">
-									<text class="cuIcon-upload"></text> 修改</button> -->
+								<button class="cu-btn round bg-green shadow" style="background-color: #A5673F;"
+									@click="deletegroup(index)">
+									<text class="cuIcon-upload"></text> 删除</button>
 							</view>
 						</view>
+						<view class="cu-list menu"
+							:class="[menuBorder?'sm-border':'',menuCard?'card-menu margin-top':'']">
 
-					</view>
-					<view class="box">
-						<view class="cu-bar btn-group">
-							<button class="cu-btn bg-green shadow-blur round lg" @click="clickcreateID()">新建账号</button>
+
+
+							<view class="cu-item" :class="menuArrow?'arrow':''" v-for="(item,index) in IDlist"
+								:key="index" v-if="item.group==group">
+								<view class="content">
+									<text class="cuIcon-btn text-green"></text>
+									<text class="text-grey">{{item.name}}</text>
+									<text class="text-grey" style="margin-left: 30rpx;">{{item.account}}</text>
+								</view>
+								<view class="action">
+									<button class="cu-btn round bg-green shadow" style="background-color: #880000;"
+										@click="clickdelete(item._id)">
+										<text class="cuIcon-upload"></text> 删除</button>
+								</view>
+								<view class="action">
+									<button class="cu-btn round bg-green shadow"
+										@click="clickchangeID(item._id,item.account,item.name,item.psw,item.group)">
+										<text class="cuIcon-upload"></text> 编辑</button>
+								</view>
+							</view>
 						</view>
-
 					</view>
+
+
+					<!-- <view class="box">
+			<view class="cu-bar btn-group">
+				<button class="cu-btn bg-green shadow-blur round lg" @click="clickcreateID()">新建账号</button>
+			</view>
+
+		</view> -->
 
 				</scroll-view>
 
@@ -57,25 +87,46 @@
 </template>
 <script>
 	export default {
-		onLoad() {
-			//调用云函数
-			wx.cloud.callFunction({
-					name: 'getData',
-					data: {
-						databaseName: 'admin',
-					}
-				})
+		onShow() {
+			// 取组
+			wx.cloud.database().collection('group').orderBy("name",'asc')
+			.get()
 				.then(res => {
-					console.log('云函数调用成功', res)
-					console.log(res.result.data)
-					this.IDlist = res.result.data
+					console.log('管理员列表请求成功', res)
+					console.log("res is" + res.data)
+					let temp = res.data;
+					let templist = [];
+					let tempIDlist = [];
+					for (let item of temp) {
+						templist.push(item.name)
+					}
+					this.grouplist = templist
+					for (let item1 of temp) {
+						tempIDlist.push(item1._id)
+					}
+					this.groupIDlist = tempIDlist
 				})
 				.catch(err => {
-					console.log('云函数调用失败', err)
+					console.log('管理员列表请求失败', err)
 				})
+			console.log("getlist")
+			wx.cloud.database().collection('admin').get()
+				.then(res => {
+					console.log('管理员列表请求成功', res)
+					this.IDlist = res.data;
+				})
+				.catch(err => {
+					console.log('管理员列表请求失败', err)
+				})
+			console.log("launched")
 		},
 		data() {
 			return {
+				groupIDlist: [],
+				newgroupname: "",
+				showPop: false, //弹窗
+				grouplist: [],
+				groupnow: "none",
 				funclist: [
 					"新增账号", "账号查看"
 				],
@@ -83,6 +134,19 @@
 				scrollLeft: 0,
 				index: -1,
 				IDlist: [],
+				// IDlist:[
+
+				// ],
+				// [{
+				// 	name:"孙福杰",
+				// 	accout:15521447381,
+				// 	psw:123,
+				// },
+				// {
+				// 	name:"黄宇勤",
+				// 	accout:1123123381,
+				// 	psw:123,
+				// }]
 
 
 				modalName: null,
@@ -90,75 +154,123 @@
 				textareaBValue: ''
 			};
 		},
-
 		methods: {
+			//删除分组
+			deletegroup(index) {
+				let that=this
+				wx.showModal({
+					cancelColor: 'cancelColor',
+					title: "是否确定删除",
+					content: "删除分组会删除对应员工，请确认所有员工已迁移到其他分组后再进行删除",
+					success(res) {
+						if (res.confirm == true) {
+							console.log('点击确认删除分组', res)
+							wx.cloud.database().collection('group')
+								.doc(that.groupIDlist[index])
+								//数据的唯一标识，也就是数据ID
+								.remove().then(res => {
+									console.log(res);
+								}).catch(err => {
+									console.log(err);
+								})
+							uni.reLaunch({
+								url: './admin'
+							})
+						}else if(res.cancel==true){
+							console.log('点击取消',res)
+						}
+					}
+				})
+			},
+			clickcreateGroup() {
+				this.showPop = !this.showPop;
+			},
+			//弹窗
+			confirmPop() { //确认
+				console.log('点击了确定')
+				this.showPop = false
+				wx.cloud.database().collection('group').add({
+					// data 字段表示需新增的 JSON 数据
+					data: {
+						// _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
 
-			clickchangeID(account) {
+						name: this.newgroupname
+
+					},
+					success: function(res) {
+						// res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+						console.log(res)
+						uni.navigateTo({
+							url: 'admin'
+						})
+					}
+				})
+			},
+			cancelPop() { //取消
+				console.log('点击了取消')
+				this.showPop = false
+			},
+			// 删除
+			clickdelete(id) {
+				wx.showModal({
+					cancelColor: 'cancelColor',
+					title: "是否确定删除",
+					content: "删除后数据不可恢复，您确定要删除该用户吗？",
+					success(res) {
+						if (res.confirm == true) {
+							console.log('点击确认删除', res)
+							wx.cloud.database().collection('admin')
+								.doc(id)
+								//数据的唯一标识，也就是数据ID
+								.remove().then(res => {
+									console.log(res);
+								}).catch(err => {
+									console.log(err);
+								})
+							uni.reLaunch({
+								url: './admin'
+							})
+						}else if(res.cancel==true){
+							console.log('点击取消',res)
+						}
+					}
+				})
+			},
+			clickchangeID(id, ac, name_db, psw_db, group) {
 				uni.navigateTo({
 					url: './changeID'
 				})
+				uni.setStorage({
+					key: "change_id",
+					data: id
+				})
+				uni.setStorage({
+					key: "change_ac",
+					data: ac
+				})
+				uni.setStorage({
+					key: "change_name",
+					data: name_db
+				})
+				uni.setStorage({
+					key: "change_psw",
+					data: psw_db
+				})
+				uni.setStorage({
+					key: "change_group",
+					data: group
+				})
+
 			},
 			clickcreateID() {
 				uni.navigateTo({
 					url: './createID'
 				})
 			},
-			// clickremoveID() {
-			// 	uni.navigateTo({
-			// 		url: './createID'
-			// 	})
-			// },
 			PickerChange(e) {
 				this.index = e.detail.value
 			},
-			MultiChange(e) {
-				this.multiIndex = e.detail.value
-			},
 
-			TimeChange(e) {
-				this.time = e.detail.value
-			},
-			DateChange(e) {
-				this.date = e.detail.value
-			},
-			RegionChange(e) {
-				this.region = e.detail.value
-			},
-			SwitchA(e) {
-				this.switchA = e.detail.value
-			},
-			SwitchB(e) {
-				this.switchB = e.detail.value
-			},
-			SwitchC(e) {
-				this.switchC = e.detail.value
-			},
-			SwitchD(e) {
-				this.switchD = e.detail.value
-			},
-			RadioChange(e) {
-				this.radio = e.detail.value
-			},
-			CheckboxChange(e) {
-				var items = this.checkbox,
-					values = e.detail.value;
-				for (var i = 0, lenI = items.length; i < lenI; ++i) {
-					items[i].checked = false;
-					for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-						if (items[i].value == values[j]) {
-							items[i].checked = true;
-							break
-						}
-					}
-				}
-			},
-
-			ViewImage(e) {
-				uni.previewImage({
-					urls: this.imgList,
-					current: e.currentTarget.dataset.url
-				});
-			},
 
 			tabSelect(e) {
 				this.TabCur = e.currentTarget.dataset.id;
